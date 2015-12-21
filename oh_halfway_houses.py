36baf6b676f9ad61613540454592660d65400a75
aliyarahman <aliya.rahman@yahoo.com>
2015-12-20 20:25:51 -0500

    Added house objects with name fields

--- oh_halfway_houses.py
+++ oh_halfway_houses.py
@@ -1,7 +1,7 @@
-# Imports lxml's HTML parsing tools
+# Imports lxml's HTML parser, regex, and request tools
 import lxml.html
 import requests
-
+import re
 
 # Grabs all HTML from the page (Ohio DRC's Halfway Houses directory by region) and converts to a tree
 url = "http://www.drc.ohio.gov/Public/HalfwayHouse/hwh_regions.htm"
@@ -9,8 +9,13 @@ page = requests.get(url)
 tree = lxml.html.fromstring(page.content)
 
 
-# Pulls the counties from the body of the page
+# Gets the counties from the body of the page
 counties = tree.xpath('//h4/text()')
 
-for c in counties:
-  print c
+# Gets the HWH names
+houses_dirty = tree.xpath("//table//tr[1]//a/text()")
+
+for h in houses_dirty:
+     house = {}
+     house['name'] = re.sub('\s+',' ',h).strip()
+     print house['name']
